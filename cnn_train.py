6d3042c12cd58fa6117f42e12ae45677b7c5e3fc
shimada <gct3860@gmail.com>
2018-04-30 12:37:23 +0900

    Organize cnn_train.py

--- cnn_train.py
+++ cnn_train.py
@@ -1,43 +1,41 @@
 # coding: utf-8
-import csv
 import time
 
 import chainer
 import chainer.functions as F
+import chainer.links as L
 import numpy as np
+import pandas as pd
+from chainer import Chain, Variable
 from chainer import cuda
 from chainer import optimizers
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 
-batchsize = 100
-n_epoch = 100
 
-dname = "raw_data"
+class CNN(Chain):
+    def __init__(self):
+        super(CNN, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(None, 20, 5)
+            self.conv2 = L.Convolution2D(None, 50, 5)
+            self.l1 = L.Linear(None, 500)
+            self.l2 = L.Linear(None, 2)
 
-f = open(dname + "/train_data.csv", "rb")
-list = []
-dataReader = csv.reader(f)
+    def __call__(self, x):
+        h = F.max_pooling_2d(F.relu(self.conv1(x)), 2)
+        h = F.max_pooling_2d(F.relu(self.conv2(h)), 2)
+        h = F.dropout(F.relu(self.l1(h)), 0.1)
+        h = self.l2(h)
+        return h
 
-for row in dataReader:
-    list.append(row)
 
-f.close()
+batchsize = 64
+n_epoch = 30
 
-np_train = np.array(list)
+df_train = pd.read_csv('./input/raw_data/train_data.csv')
+X = df_train.iloc[:, 2:].astype(np.float32).values
+y = (df_train.iloc[:, 1].astype(np.int32) - 2).values
 
-X = np_train[:, 2:].astype(np.float32)[:]
-y = np_train[:, 1].astype(np.int32)[:]
-
-y[y == 2] = 0
-y[y == 3] = 1
-
-gpu_flag = -1
-
-if gpu_flag >= 0:
-    cuda.check_cuda_available()
-xp = cuda.cupy if gpu_flag >= 0 else np
-
-# ピクセルの値を0.0-1.0に正規化
 X /= X.max()
 
 # 訓練データとテストデータに分割
@@ -47,45 +45,20 @@ N = y_train.size
 N_test = y_test.size
 
 # 画像を (nsample, channel, height, width) の4次元テンソルに変換
-# MNISTはチャンネル数が1なのでreshapeだけでOK
 X_train = X_train.reshape((len(X_train), 1, 32, 96))
 X_test = X_test.reshape((len(X_test), 1, 32, 96))
-# plt.imshow(X_train[1][0], cmap=pylab.cm.gray_r, interpolation='nearest')
-# plt.show()
 
-model = chainer.FunctionSet(conv1=F.Convolution2D(1, 20, 5),
-                            # 入力1枚、出力20枚、フィルタサイズ5ピクセル
-                            conv2=F.Convolution2D(20, 50, 5),
-                            # 入力20枚、出力50枚、フィルタサイズ5ピクセル
-                            l1=F.Linear(5250, 500),  # 入力800ユニット、出力500ユニット
-                            l2=F.Linear(500, 2))  # 入力500ユニット、出力10ユニット
+model = CNN()
+
+gpu_flag = -1
 
 if gpu_flag >= 0:
-    cuda.get_device(gpu_flag).use()
+    cuda.get_device_from_id(gpu_flag).use()
     model.to_gpu()
+xp = np if gpu_flag < 0 else cuda.cupy
 
-
-def forward(x_data, y_data, train=True):
-    x, t = chainer.Variable(x_data), chainer.Variable(y_data)
-    h = F.max_pooling_2d(F.relu(model.conv1(x)), 2)
-    h = F.max_pooling_2d(F.relu(model.conv2(h)), 2)
-    h = F.dropout(F.relu(model.l1(h)), train=train)
-    y = model.l2(h)
-    if train:
-        return F.softmax_cross_entropy(y, t)
-    else:
-        return F.accuracy(y, t)
-
-
-optimizer = optimizers.Adam()
+optimizer = optimizers.MomentumSGD(lr=0.001)
 optimizer.setup(model)
-
-fp1 = open("accuracy.txt", "w")
-fp2 = open("loss.txt", "w")
-
-fp1.write("epoch\ttest_accuracy\n")
-fp2.write("epoch\ttrain_loss\n")
-
 # 訓練ループ
 start_time = time.clock()
 for epoch in range(1, n_epoch + 1):
@@ -94,39 +67,38 @@ for epoch in range(1, n_epoch + 1):
     perm = np.random.permutation(N)
     sum_loss = 0
     for i in range(0, N, batchsize):
-        x_batch = xp.asarray(X_train[perm[i:i + batchsize]])
-        y_batch = xp.asarray(y_train[perm[i:i + batchsize]])
+        x_batch = Variable(xp.asarray(X_train[perm[i:i + batchsize]]))
+        y_batch = Variable(xp.asarray(y_train[perm[i:i + batchsize]]))
 
-        optimizer.zero_grads()
-        loss = forward(x_batch, y_batch)
+        model.cleargrads()
+
+        loss = F.softmax_cross_entropy(model(x_batch), y_batch)
+
         loss.backward()
+
         optimizer.update()
-        sum_loss += float(loss.data) * len(y_batch)
+        model.cleargrads()
+
+        sum_loss += float(loss.array) * len(x_batch)
 
     print "train mean loss: %f" % (sum_loss / N)
-    fp2.write("%d\t%f\n" % (epoch, sum_loss / N))
-    fp2.flush()
 
-    sum_accuracy = 0
-    for i in range(0, N_test, batchsize):
-        x_batch = xp.asarray(X_test[i:i + batchsize])
-        y_batch = xp.asarray(y_test[i:i + batchsize])
+    with chainer.using_config('train', True):
+        sum_accuracy = 0
+        for i in range(0, N_test, batchsize):
+            x_batch = xp.asarray(X_test[i:i + batchsize])
+            y_batch = xp.asarray(y_test[i:i + batchsize])
 
-        acc = forward(x_batch, y_batch, train=False)
-        sum_accuracy += float(acc.data) * len(y_batch)
+            acc = F.accuracy(model(x_batch), y_batch, )
+            sum_accuracy += float(acc.data) * len(y_batch)
 
     print "test accuracy: %f" % (sum_accuracy / N_test)
-    fp1.write("%d\t%f\n" % (epoch, sum_accuracy / N_test))
-    fp1.flush()
 
 end_time = time.clock()
 print end_time - start_time
 
-fp1.close()
-fp2.close()
-
-import cPickle
+from chainer.serializers import save_npz
 
 # CPU環境でも学習済みモデルを読み込めるようにCPUに移してからダンプ
 model.to_cpu()
-cPickle.dump(model, open("model.pkl", "wb"), -1)
+save_npz("output/cnn_model.pkl", model)
